b9c3a044d46ec146a7dd5b79749e4a39d9fc3202
arttkachev <art.tkachev@bk.ru>
2022-02-08 00:24:12 +0300

    code cleanup

--- reflection-system/reflection-system.cpp
+++ reflection-system/reflection-system.cpp
@@ -5,9 +5,7 @@
 int main() {
   Aircraft const Airbus{ "Airbus", "A320", 2001 };
   Aircraft const Boeing{ "Boeing", "B737", 2009 };
-  std::cout << GetType(Airbus);
-  std::cout << std::endl;
-  std::cout << Airbus;
+  std::cout << GetType(Airbus) << std::endl << Airbus;
   int exitCode{};
   while (exitCode != 1) {
     std::cin >> exitCode;
